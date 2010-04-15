67fb6be679f0f97414c3147df4a23de9c5ed2755
Mathias Lin <mail@mathiaslin.com>
2010-04-15 15:15:47 +0000

    * dump was missing some information like autoincrement, indexes, constraints -> that's now added * switched from manual creation to automatic structure creation using 'SHOW CREATE TABLE xyz' -> backup module currently only working * with MySql changed/solved encoding issue (-> utf8) ** (Judy Wawira is currently testing this release before announcing it.)

--- web/src/org/openmrs/module/databasebackup/web/util/DbDump.java
+++ web/src/org/openmrs/module/databasebackup/web/util/DbDump.java
@@ -15,6 +15,7 @@ package org.openmrs.module.databasebackup.web.util;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
@@ -40,8 +41,10 @@ public class DbDump {
 	/** Logger for this class and subclasses */
 	protected final static Log log = LogFactory.getLog(DbDump.class);
 	
+	private static final String fileEncoding = "UTF8";
+	
     /** Dump the whole database to an SQL string */
-    public static void dumpDB(Properties props) {
+    public static void dumpDB(Properties props) throws Exception {
     	String filename = props.getProperty("filename");
     	String folder= props.getProperty("folder");
         String driverClassName = props.getProperty("driver.class");
@@ -50,149 +53,155 @@ public class DbDump {
         String columnNameQuote = props.getProperty("columnName.quoteChar", "");
         DatabaseMetaData dbMetaData = null;
         Connection dbConn = null;
-        try {
-            Class.forName(driverClassName);
-            dbConn = DriverManager.getConnection(driverURL, props);
-            dbMetaData = dbConn.getMetaData();
+
+        Class.forName(driverClassName);
+        dbConn = DriverManager.getConnection(driverURL, props);
+        dbMetaData = dbConn.getMetaData();
+        
+        FileOutputStream fos = new FileOutputStream(folder + filename);        
+        OutputStreamWriter result = new OutputStreamWriter(fos, fileEncoding);            
+        
+        String catalog = props.getProperty("catalog");
+        String schema = props.getProperty("schemaPattern");
+        
+        String tablesIncluded = props.getProperty("tables.included");
+        List<String> tablesIncludedVector = Arrays.asList(tablesIncluded.split(","));
+
+        String tablesExcluded = props.getProperty("tables.excluded");
+        List<String> tablesExcludedVector = Arrays.asList(tablesExcluded.split(","));
+
+        ResultSet rs = dbMetaData.getTables(catalog, schema, null, null);
+        int progressCnt = 0;
+
+        log.debug("tablesIncluded: " + tablesIncluded);
+        log.debug("tablesExcluded: " + tablesExcluded);
+
+        result.write( "/*\n" + 
+        		" * DB jdbc url: " + driverURL + "\n" +
+        		" * Database product & version: " + dbMetaData.getDatabaseProductName() + " " + dbMetaData.getDatabaseProductVersion() + "\n" +
+        		" */"
+        		);                                   
+        
+        List<String> tableVector = new Vector<String>();
+        int progressTotal = 0;
+        while(rs.next()) {
+            String tableName = rs.getString("TABLE_NAME");
+            if (
+                    ( tablesIncluded.contains("all")&&!tablesExcludedVector.contains(tableName)||tablesIncluded.contains(tableName) )
+                    || ( tablesExcludedVector.contains("none")&&!tablesIncludedVector.contains("none") )
+                    ) {
+                progressTotal++;
+                tableVector.add(tableName);
+            }                
         }
-        catch( Exception e ) {
-            System.err.println("Unable to connect to database: "+e);
-            //return null;
-        }
+        rs.beforeFirst();
+        
+        if (! rs.next()) {
+            log.error("Unable to find any tables matching: catalog="+catalog+" schema=" + schema + " tables=" + tableVector.toArray().toString());
+            rs.close();
+        } else {
+            do {
+                String tableName = rs.getString("TABLE_NAME");                    
+                String tableType = rs.getString("TABLE_TYPE");
+                
+                if (tableVector.contains(tableName)) {
 
-        try {
-            FileOutputStream result = new FileOutputStream(folder + filename);
-                        
-            String catalog = props.getProperty("catalog");
-            String schema = props.getProperty("schemaPattern");
-            
-            String tablesIncluded = props.getProperty("tables.included");
-            List<String> tablesIncludedVector = Arrays.asList(tablesIncluded.split(","));
+                	progressCnt++;
+                	BackupFormController.getProgressInfo().put(filename, "Backing up table " + progressCnt + " of " + progressTotal + " (" + tableName + ")...");                    	
+                	
+                    if ("TABLE".equalsIgnoreCase(tableType)) {
 
-            String tablesExcluded = props.getProperty("tables.excluded");
-            List<String> tablesExcludedVector = Arrays.asList(tablesExcluded.split(","));
-
-            ResultSet rs = dbMetaData.getTables(catalog, schema, null, null);
-            int progressCnt = 0;
-
-            log.debug("tablesIncluded: " + tablesIncluded);
-            log.debug("tablesExcluded: " + tablesExcluded);
-
-            List<String> tableVector = new Vector<String>();
-            int progressTotal = 0;
-            while(rs.next()) {
-                String tableName = rs.getString("TABLE_NAME");
-                if (
-                        ( tablesIncluded.contains("all")&&!tablesExcludedVector.contains(tableName)||tablesIncluded.contains(tableName) )
-                        || ( tablesExcludedVector.contains("none")&&!tablesIncludedVector.contains("none") )
-                        ) {
-                    progressTotal++;
-                    tableVector.add(tableName);
-                }                
-            }
-            rs.beforeFirst();
-            
-            if (! rs.next()) {
-                log.error("Unable to find any tables matching: catalog="+catalog+" schema=" + schema + " tables=" + tableVector.toArray().toString());
-                rs.close();
-            } else {
-                do {
-                    String tableName = rs.getString("TABLE_NAME");                    
-                    String tableType = rs.getString("TABLE_TYPE");
-                    
-                    if (tableVector.contains(tableName)) {
-
-                    	progressCnt++;
-                    	BackupFormController.getProgressInfo().put(filename, "Backing up table " + progressCnt + " of " + progressTotal + " (" + tableName + ")...");                    	
+                    	result.write( "\n\n-- Structure for table `" + tableName + "`\n" );
+                    	result.write( "DROP TABLE IF EXISTS `"+tableName+"`;\n" );
                     	
-	                    if ("TABLE".equalsIgnoreCase(tableType)) {
+                    	PreparedStatement tableStmt = dbConn.prepareStatement("SHOW CREATE TABLE "+ tableName +";");
+                    	ResultSet tablesRs = tableStmt.executeQuery();
+                    	while (tablesRs.next()) {
+                    		result.write(tablesRs.getString("Create Table") + "\n\n");	
+                    	}
+                    	tablesRs.close();
+                    	tableStmt.close();
+                    	
+                    	/*
+                    	result.write( "\nCREATE TABLE "+tableName+" (\n" );
+                        ResultSet tableMetaData = dbMetaData.getColumns(null, null, tableName, "%");	                        
+                        boolean firstLine = true;
+                        while (tableMetaData.next()) {
+                            if (firstLine) {
+                                firstLine = false;
+                            } else {
+                                // If not the first line, then finish the previous line with a comma
+                            	result.write( ",\n" );
+                            }
+                            String columnName = tableMetaData.getString("COLUMN_NAME");
+                            String columnType = tableMetaData.getString("TYPE_NAME");
+                            // WARNING: this may give daft answers for some types on some databases (eg JDBC-ODBC link)
+                            int columnSize = tableMetaData.getInt("COLUMN_SIZE");
+                            String nullable = tableMetaData.getString("IS_NULLABLE");
+                            String nullString = "NULL";
+                            if ("NO".equalsIgnoreCase(nullable)) {
+                                nullString = "NOT NULL";
+                            }
+                            result.write( "    " + columnNameQuote + columnName +columnNameQuote + " " + columnType + " (" + columnSize + ")" + " " + nullString );
+                        }
+                        tableMetaData.close();
 
-	                    	result.write( ("\n\n-- "+tableName).getBytes() );
-	                    	result.write( ("\nCREATE TABLE "+tableName+" (\n").getBytes() );
-	                        ResultSet tableMetaData = dbMetaData.getColumns(null, null, tableName, "%");	                        
-	                        boolean firstLine = true;
-	                        while (tableMetaData.next()) {
-	                            if (firstLine) {
-	                                firstLine = false;
-	                            } else {
-	                                // If not the first line, then finish the previous line with a comma
-	                            	result.write( (",\n").getBytes() );
-	                            }
-	                            String columnName = tableMetaData.getString("COLUMN_NAME");
-	                            String columnType = tableMetaData.getString("TYPE_NAME");
-	                            // WARNING: this may give daft answers for some types on some databases (eg JDBC-ODBC link)
-	                            int columnSize = tableMetaData.getInt("COLUMN_SIZE");
-	                            String nullable = tableMetaData.getString("IS_NULLABLE");
-	                            String nullString = "NULL";
-	                            if ("NO".equalsIgnoreCase(nullable)) {
-	                                nullString = "NOT NULL";
-	                            }
-	                            result.write( ("    "+columnNameQuote+columnName+columnNameQuote+" "+columnType+" ("+columnSize+")"+" "+nullString).getBytes() );
-	                        }
-	                        tableMetaData.close();
-	
-	                        // Now we need to put the primary key constraint
-	                        try {
-	                            ResultSet primaryKeys = dbMetaData.getPrimaryKeys(catalog, schema, tableName);
-	                            String primaryKeyName = null;
-	                            StringBuffer primaryKeyColumns = new StringBuffer();
-	                            while (primaryKeys.next()) {
-	                                String thisKeyName = primaryKeys.getString("PK_NAME");
-	                                if ((thisKeyName != null && primaryKeyName == null)
-	                                        || (thisKeyName == null && primaryKeyName != null)
-	                                        || (thisKeyName != null && ! thisKeyName.equals(primaryKeyName))
-	                                        || (primaryKeyName != null && ! primaryKeyName.equals(thisKeyName))) {
-	                                    if (primaryKeyColumns.length() > 0) {
-	                                        result.write(",\n    PRIMARY KEY ".getBytes());
-	                                        if (primaryKeyName != null) { result.write(primaryKeyName.getBytes()); }
-	                                        result.write( ("("+primaryKeyColumns.toString()+")").getBytes() );
-	                                    }
-	                                    // Start again with the new name
-	                                    primaryKeyColumns = new StringBuffer();
-	                                    primaryKeyName = thisKeyName;
-	                                }
-	                                // Now append the column
-	                                if (primaryKeyColumns.length() > 0) {
-	                                    primaryKeyColumns.append(", ");
-	                                }
-	                                primaryKeyColumns.append(primaryKeys.getString("COLUMN_NAME"));
-	                            }
-	                            if (primaryKeyColumns.length() > 0) {
-	                                result.write(",\n    PRIMARY KEY ".getBytes());
-	                                if (primaryKeyName != null) { result.write(primaryKeyName.getBytes()); }
-	                                result.write( (" ("+primaryKeyColumns.toString()+")").getBytes() );
-	                            }
-	                            primaryKeys.close();
-	                        } catch (SQLException e) {
-	                        	log.error("Unable to get primary keys for table "+tableName+".", e);
-	                        }
+                        // primary key constraint
+                        try {
+                            ResultSet primaryKeys = dbMetaData.getPrimaryKeys(catalog, schema, tableName);
+                            String primaryKeyName = null;
+                            StringBuffer primaryKeyColumns = new StringBuffer();
+                            while (primaryKeys.next()) {
+                                String thisKeyName = primaryKeys.getString("PK_NAME");
+                                if ((thisKeyName != null && primaryKeyName == null)
+                                        || (thisKeyName == null && primaryKeyName != null)
+                                        || (thisKeyName != null && ! thisKeyName.equals(primaryKeyName))
+                                        || (primaryKeyName != null && ! primaryKeyName.equals(thisKeyName))) {
+                                    if (primaryKeyColumns.length() > 0) {
+                                        result.write(",\n    PRIMARY KEY ");
+                                        if (primaryKeyName != null) { result.write(primaryKeyName); }
+                                        result.write( "("+primaryKeyColumns.toString()+")" );
+                                    }
+                                    // Start again with the new name
+                                    primaryKeyColumns = new StringBuffer();
+                                    primaryKeyName = thisKeyName;
+                                }
+                                // append the column
+                                if (primaryKeyColumns.length() > 0) {
+                                    primaryKeyColumns.append(", ");
+                                }
+                                primaryKeyColumns.append(primaryKeys.getString("COLUMN_NAME"));
+                            }
+                            if (primaryKeyColumns.length() > 0) {
+                                result.write(",\n    PRIMARY KEY ");
+                                if (primaryKeyName != null) { result.write(primaryKeyName); }
+                                result.write( " ("+primaryKeyColumns.toString()+")" );
+                            }
+                            primaryKeys.close();
+                        } catch (SQLException e) {
+                        	log.error("Unable to get primary keys for table "+tableName+".", e);
+                        }
 
-	                        result.write("\n);\n".getBytes());
+                        result.write("\n);\n");
+                        */
 
-	                        dumpTable(dbConn, result, tableName);
-	                        System.gc();
-	                    }
+                        dumpTable(dbConn, result, tableName);
+                        System.gc();
                     }
-                } while (rs.next());
-                rs.close();
-            }
-            
-            result.flush();
-            result.close();
-            
-            dbConn.close();
-            //return result.toString();
-        } catch (SQLException e) {
-            log.error("An error occured. ", e);  //To change body of catch statement use Options | File Templates.
-        } catch (IOException e) {
-            log.error("An error occured. ", e);  //To change body of catch statement use Options | File Templates.
+                }
+            } while (rs.next());
+            rs.close();
         }
-        //return null;
+        
+        result.flush();
+        result.close();
+        
+        dbConn.close();       
     }
 
     /** dump this particular table to the string buffer */
     //private static void dumpTable(Connection dbConn, StringBuffer result, String tableName) {
-	private static void dumpTable(Connection dbConn, FileOutputStream result, String tableName) {
+	private static void dumpTable(Connection dbConn, OutputStreamWriter result, String tableName) {
         try {
             // create table sql
             PreparedStatement stmt = dbConn.prepareStatement("SELECT * FROM "+tableName);
@@ -201,23 +210,23 @@ public class DbDump {
             int columnCount = metaData.getColumnCount();
 
             // data inserts
-            result.write( ("\n\n-- Data for "+tableName+"\n").getBytes() );
+            result.write( "\n\n-- Data for table '"+tableName+"'\n" );
             while (rs.next()) {
-                result.write( ("INSERT INTO "+tableName+" VALUES (").getBytes() );
+                result.write( "INSERT INTO "+tableName+" VALUES (" );
                 for (int i=0; i<columnCount; i++) {
                     if (i > 0) {
-                        result.write(", ".getBytes());
+                        result.write(", ");
                     }
                     Object value = rs.getObject(i+1);
                     if (value == null) {
-                        result.write("NULL".getBytes());
+                        result.write("NULL");
                     } else {
                         String outputValue = value.toString();
                         outputValue = outputValue.replaceAll("'","\\'");
-                        result.write( ("'"+outputValue+"'").getBytes() );
+                        result.write( "'"+outputValue+"'" );
                     }
                 }
-                result.write(");\n".getBytes());
+                result.write(");\n");
             }
             rs.close();
             stmt.close();
